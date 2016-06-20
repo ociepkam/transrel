de7c3cd9625135bc9b3a370aa43142a23bf02c0d
ociepkam <ociepka.m@gmail.com>
2016-06-20 17:24:38 +0200

    remove uppercases greek letters

--- classes/trial.py
+++ classes/trial.py
@@ -90,11 +90,7 @@ numbers = list(set("0123456789"))
 
 greek_letters = [u'\u0391', u'\u0392', u'\u0393', u'\u0394', u'\u0395', u'\u0396', u'\u0397', u'\u0398', u'\u0399',
                  u'\u039A', u'\u039B', u'\u039C', u'\u039D', u'\u039E', u'\u039F', u'\u03A0', u'\u03A1', u'\u03A2',
-                 u'\u03A3', u'\u03A4', u'\u03A5', u'\u03A6', u'\u03A7', u'\u03A8', u'\u03A9',
-                 # uppercases
-                 u'\u03B1', u'\u03B2', u'\u03B3', u'\u03B4', u'\u03B5', u'\u03B6', u'\u03B7', u'\u03B8', u'\u03B9',
-                 u'\u03BA', u'\u03BB', u'\u03BC', u'\u03BD', u'\u03BE', u'\u03BF', u'\u03C0', u'\u03C1', u'\u03C2',
-                 u'\u03C3', u'\u03C4', u'\u03C5', u'\u03C6', u'\u03C7', u'\u03C8', u'\u03C9']
+                 u'\u03A3', u'\u03A4', u'\u03A5', u'\u03A6', u'\u03A7', u'\u03A8', u'\u03A9']
 
 
 class Trial:
